e95b1ca4cf34309b270900a6fb8ef17b6f65820b
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-27 16:22:25 +0100

    Refactor: Blog is now on blog.html and home.html is for company landing page

--- animalmagicblog.py
+++ animalmagicblog.py
@@ -20,8 +20,12 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Us')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', posts=posts)
