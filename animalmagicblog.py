d3b29b5b04fcf4b6054af510038d71965fe01b29
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-27 15:20:47 +0100

    Home and About Us pages have titles

--- animalmagicblog.py
+++ animalmagicblog.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'James Drysdale', 
+        'title': 'Feeding birds',
+        'content': 'Why you are probably feeding birds to death and how to feed them the right way.',
+        'date_posted': 'September 1st 2020'
+    },
+    {
+        'author': 'James Drysdale', 
+        'title': 'Dog Training 101: Recall',
+        'content': 'How to get your dog to return to you on your command.',
+        'date_posted': 'September 11th 2020'
+    }
+
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About Us')
