5a632439f54650b0118017eaebdad186ff4b6108
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-27 14:35:49 +0100

    App has rout for Home Page and About Us page

--- animalmagicblog.py
+++ animalmagicblog.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    return "<h1>Home Page</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>About Us</h1>" 
