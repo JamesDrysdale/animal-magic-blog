6084be1234b812620cf38e4fd268594fd7358cc9
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-27 22:14:19 +0100

    Passwords have a minimum length

--- forms.py
+++ forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)]) # Usernames must be 2 to 20 characters long
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)]) # Min password length is 6
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
