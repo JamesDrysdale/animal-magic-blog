101d2f23d21c8cce0939e132485e4c2c99bb1b82
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-27 22:06:55 +0100

    Register Page created

--- animalmagicblog.py
+++ animalmagicblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1a394f30fa58662edd8941ed9f8c21e2'
 
 posts = [
     {
@@ -29,3 +32,23 @@ def about():
 @app.route('/blog')
 def blog():
     return render_template('blog.html', posts=posts, title='Blog')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
