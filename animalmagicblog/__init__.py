7c3750a1d03ecc69ce1e91d7dfec9634632ab71a
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-28 17:28:48 +0100

    Site now requires flask-login LoginManager

--- animalmagicblog/__init__.py
+++ animalmagicblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 # Configuration
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '1a394f30fa58662edd8941ed9f8c21e2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from animalmagicblog import routes # Import here to avoid circular import error 
