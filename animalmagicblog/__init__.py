630d31b629bba4d7ddd02667abac5628f737b426
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-28 11:46:56 +0100

    Refactor code into a package structure

---
+++ animalmagicblog/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Configuration
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1a394f30fa58662edd8941ed9f8c21e2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from animalmagicblog import routes # Import here to avoid circular import error 
--- animalmagicblog/routes.py
+++ animalmagicblog/routes.py
@@ -1,37 +1,7 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-# Configuration
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '1a394f30fa58662edd8941ed9f8c21e2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-# Models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(20), unique=True, nullable=False) # Max 20 characters
-    email =  db.Column(db.String(120), unique=True, nullable=False)
-    image_file =  db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    # 1 to many relationship between user/author and posts
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
+from flask import render_template, url_for, flash, redirect
+from animalmagicblog import app
+from animalmagicblog.forms import RegistrationForm, LoginForm
+from animalmagicblog.models import User, Post
 
 # Dummy Data for testing
 posts = [
@@ -47,7 +17,6 @@ posts = [
         'content': 'How to get your dog to return to you on your command.',
         'date_posted': 'September 11th 2020'
     }
-
 ]
 
 @app.route('/')
@@ -70,7 +39,6 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
-
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
