9cda5bcae564f41395deb8cf9658c8f55d29a630
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-28 19:26:54 +0100

    Created form to allow users to update account details

--- animalmagicblog/forms.py
+++ animalmagicblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from animalmagicblog.models import User
@@ -15,6 +16,33 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is unavailable, please choose another one.')
+ 
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user: 
+                raise ValidationError('That email is unavailable, please choose another one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)]) # Usernames must be 2 to 20 characters long
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is unavailable, please choose another one.')
@@ -23,11 +51,3 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user: 
             raise ValidationError('That email is unavailable, please choose another one.')
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
--- animalmagicblog/routes.py
+++ animalmagicblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from animalmagicblog import app, db, bcrypt
-from animalmagicblog.forms import RegistrationForm, LoginForm
+from animalmagicblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from animalmagicblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -70,5 +70,7 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = (url_for('static', filename='profile_pics/' + current_user.image_file))
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', 
+                            image_file=image_file, form=form)
