bbe87e8a8df46751a6e31a0dadc20abfd6baf2ae
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-28 17:55:11 +0100

    User is redirected to home screen, if they are logged in and select the login or register links

--- animalmagicblog/models.py
+++ animalmagicblog/models.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from animalmagicblog import db
+from animalmagicblog import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Models
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), unique=True, nullable=False) # Max 20 characters
     email =  db.Column(db.String(120), unique=True, nullable=False)
--- animalmagicblog/routes.py
+++ animalmagicblog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from animalmagicblog import app, db, bcrypt
 from animalmagicblog.forms import RegistrationForm, LoginForm
 from animalmagicblog.models import User, Post
+from flask_login import login_user, current_user
 
 # Dummy Data for testing
 posts = [
@@ -34,6 +35,8 @@ def blog():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # Hashes the password to secure it
@@ -46,11 +49,14 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password': #dummy data for testing
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+        else: 
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
