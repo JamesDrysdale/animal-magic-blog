2c4cccf4fd91eb2d48b32c5446f3211f9ee73e8d
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-08-29 10:37:13 +0100

    Add route for creating a new post

--- animalmagicblog/routes.py
+++ animalmagicblog/routes.py
@@ -103,3 +103,8 @@ def account():
     image_file = (url_for('static', filename='profile_pics/' + current_user.image_file))
     return render_template('account.html', title='Account', 
                             image_file=image_file, form=form)
+
+@app.route("/post/new")
+@login_required
+def new_post():
+    return render_template('create_post.html', title='New Post')
