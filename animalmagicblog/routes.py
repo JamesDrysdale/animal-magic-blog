ce7fda014212fc8a4cd992b12a4f9c8dfa1a6518
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-09-01 12:31:47 +0100

    Route to recieve and check if a token is valid or expired

--- animalmagicblog/routes.py
+++ animalmagicblog/routes.py
@@ -154,3 +154,14 @@ def reset_request():
         return redirect(url_for('home'))
     form = RequestResetForm()
     return render_template('reset_request.html', title='Reset Password', form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form=form)
